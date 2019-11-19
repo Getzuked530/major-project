f78b7eb774b0d09f7e25727355a7dc01132e7387
JoshDexter <45461837+JoshDexter@users.noreply.github.com>
2019-11-19 14:10:19 -0600

    Text box done, next scene started

--- shrekoclock/sketch.js
+++ shrekoclock/sketch.js
@@ -4,7 +4,10 @@ let noEscape = "Options? What options? You are playing a game desinged by me. Wh
 let normalShrek;
 let swampBack;
 let showingGame = false;
-let testAudio;
+// let testAudio;
+let clickedOnTextBox = false;
+let yourName;
+let money;
   
 function setup() {
 soundFormats("mp3")
@@ -15,6 +18,7 @@ createCanvas(canvasWidth, canvasHeight)
 background(255)
 }
 
+
 class EnterText {
   constructor(x1, y1, w1, h1, maxLimit1){
     this.x = x1;
@@ -24,18 +28,67 @@ class EnterText {
     this.maxLimit = maxLimit1;
     this.textData = "text"
     
-
-
   }
   draw(){
+    push()
+    fill(255);
     rect(this.x, this.y, this.w, this.h)
-    text(textData, )
+    push()
+    fill(0);
+    text(this.textData, this.x, this.y)
+    pop()
+  }
+  inputTextName(inputText){
+    if (this.textData.length < this.maxLimit){  
+     
+      this.textData += inputText;
+      
+      
+      nameBox.draw();
+      yourName = this.textData;
+    }
+  }
+  deleteText(){
+    this.textData = this.textData.slice(0, -1);
+    nameBox.draw();
   }
 }
+function mousePressed(){
+  if (mouseX >= windowWidth/2 - 250 && mouseX <= windowWidth/2 + 500 && mouseY <= windowHeight/1.5 + 75 && mouseY >= windowHeight/1.5 - 50){
+    clickedOnTextBox = true;
+
+  }
+}
+function keyTyped(){
+  if (clickedOnTextBox){
+    nameBox.inputTextName(key);
+  }
+  
+
+
+}
+function keyPressed(){
+  if (keyCode === BACKSPACE && clickedOnTextBox){
+
+    nameBox.deleteText();
+  }
+
+  if (keyCode === ENTER && clickedOnTextBox){
+    console.log("cont to game")
+    menuState = "intro"
+  }
+}
+
+
+
+
+
+
+
 function preload(){
   normalShrek = loadImage("assets/standingShrek.png")
   swampBack = loadImage("assets/phatswamp.jpg")
-  testAudio = loadSound("assets/sharpbreath.mp3");
+  // testAudio = loadSound("assets/sharpbreath.mp3");
 }
 
 function windowResized(){
@@ -47,8 +100,10 @@ function ui(){
   showingGame = false;
 }
 function draw() {
+  
   whichMenu();
 
+  
 }
 
 function startMenu(){
@@ -85,7 +140,7 @@ function checkIfMenuButtonClicked(){
     if (mouseX > width/2 - 200 && mouseX < width/2 + 200 &&
         mouseY > height/2 - 100 - 75 && mouseY < height/2 - 100 +75){
           menuState = "startGame";
-          console.log("hecker")
+
         }
 
      // check options button
@@ -108,7 +163,7 @@ function showOptions(){
   fill("white");
   stroke("black");
   text(noEscape, width/2, height/2 - 100);
-  console.log("deaf");
+
 
   textAlign(CENTER);
   text("Like seriously what?", width/2, height/2)
@@ -122,23 +177,28 @@ function showOptions(){
 }
 
 function showGame(){
+  
   showingGame = true;
-  background(0);
-  console.log("dudud")
+  background("green");
+
   fill("white")
   text("What is your name? ", windowWidth/2, windowHeight/2);
-  nameBox = new EnterText(windowWidth/2, windowHeight/0.5, 150, 50, 1000);
+  nameBox = new EnterText(windowWidth/2, windowHeight/1.5, 500, 50, 12);
   nameBox.draw();
   
 
 }
 
+function showIntro(){
+  background(0);
+}
+  
 
 function whichMenu() {
   if (menuState === "startMenu"){
     startMenu();
     checkIfMenuButtonClicked();
-    console.log("dumbbitch")
+
   }
 
   else if (menuState === "startGame" && showingGame === false){
@@ -149,6 +209,10 @@ function whichMenu() {
   else if (menuState === "options"){
     
     showOptions();
+  }
+
+  else if (menuState ==="intro"){
+     showIntro();
   }
 }
 // function keyPressed(){
